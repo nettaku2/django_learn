d0d789cd91deed389e15f568c771057c83e041d8
Harry <choyonghee1974@gmail.com>
2021-10-04 06:41:51 +0900

    store, tags 데이터모델링, likes앱 추가 및 데이터 모델링

--- likes/models.py
+++ likes/models.py
@@ -1,20 +1,14 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 
 
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
-
-# What tag applied to what object
-
-
-class TaggedItem(models.Model):
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    # Type of object - find table
-    # ID of object - find record
+class LikedItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveBigIntegerField()
     content_object = GenericForeignKey()
