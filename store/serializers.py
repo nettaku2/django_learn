9f56efc4dc3afeb29aafc5d648894a34d40d75ee
Harry <choyonghee1974@gmail.com>
2021-10-27 08:08:06 +0900

    django

--- store/serializers.py
+++ store/serializers.py
@@ -7,7 +7,17 @@ from .models import Product, Collection
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    price = serializers.DecimalField(
+        max_digits=6, decimal_places=2, source='unit_price')
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
+    collection_id = serializers.PrimaryKeyRelatedField(
+        queryset=Collection.objects.all()
+    )
+    # collection_title = serializers.StringRelatedField()
+
+    def calculate_tax(self, product):
+        return product.unit_price * Decimal(1.1)
 
 
 class CollectionSerializer(serializers.ModelSerializer):
